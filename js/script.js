9a6f18604937989686d096c6f6100c3e5d3a7dd3
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-11-02 17:23:43 +0100

    Move note styling from js to css

--- js/script.js
+++ js/script.js
@@ -3,21 +3,14 @@
 function getNoteDiv() {
     let noteDiv = document.createElement('div');
     noteDiv.setAttribute('class', 'note');
-
-    noteDiv.style.display = 'block';
-    noteDiv.style.width = '300px';
-    noteDiv.style.height = '400px';
-    noteDiv.style.backgroundColor = '#FFF';
-
     return noteDiv;
 }
 
 function getNewNote() {
 
     let newNote = getNoteDiv();
-    newNote.appendChild(appendTopBar());
-    newNote.appendChild(appendContentArea());
-
+    // newNote.appendChild(appendTopBar());
+    // newNote.appendChild(appendContentArea());
     return newNote;
 }
 
