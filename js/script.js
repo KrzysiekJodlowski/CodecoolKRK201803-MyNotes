8b09f1dd0185f876f5869de7ec8274d99613bbdc
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-11-04 14:49:30 +0100

    Add saving and reloading notes with localStorage API

--- js/script.js
+++ js/script.js
@@ -1,5 +1,73 @@
 'use strict';
 
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+let notes = JSON.parse(localStorage.getItem('notes'));
+
+for (let index = 0; index < notes.length; index++) {
+
+    //
+    let savedNote = document.createElement('div');
+    savedNote.setAttribute('class', 'noteDiv');
+
+    let savedNoteTopBar = document.createElement('span');
+    savedNoteTopBar.setAttribute('class', 'noteTopBar');
+
+    let savedNoteTopBarTitle = document.createElement('input');
+    savedNoteTopBarTitle.setAttribute('type', 'text');
+    if (notes[index].title != "") {
+        savedNoteTopBarTitle.value = notes[index].title;
+    } else {
+        savedNoteTopBarTitle.setAttribute('placeholder', 'title');
+    }
+
+    let savedNoteTopBarDeleteButton = document.createElement('button');
+    savedNoteTopBarDeleteButton.setAttribute('class', 'delete');
+    savedNoteTopBarDeleteButton.addEventListener('click', function (event) {
+        let targetElement = event.target;
+        targetElement = targetElement.parentNode;
+        targetElement = targetElement.parentNode;
+        document.body.removeChild(targetElement);
+    }, false);
+
+    savedNoteTopBar.appendChild(savedNoteTopBarTitle);
+    savedNoteTopBar.appendChild(savedNoteTopBarDeleteButton);
+
+    savedNote.appendChild(savedNoteTopBar);
+
+
+    let savedNoteContentArea = document.createElement('textarea');
+    savedNoteContentArea.setAttribute('class', 'content');
+    if (notes[index].content != "") {
+        savedNoteContentArea.value = notes[index].content;
+    } else {
+        savedNoteContentArea.setAttribute('placeholder', 'content');
+    }
+
+
+    savedNote.appendChild(savedNoteContentArea);
+
+    let savedPositions = [notes[index].xPosition, notes[index].yPosition];
+    setDragEvent(savedNote, savedPositions);
+
+    let yPos = notes[index].yPosition;
+    let xPos = notes[index].xPosition;
+    let wdth = notes[index].width;
+    let hght = notes[index].height;
+
+    // savedNote.style.marginTop = yPos.toString() + 'px';
+    // savedNote.style.marginLeft = xPos.toString() + 'px';
+
+    savedNote.style.transform = "translateX(" +
+        xPos + "px) translateY(" +
+        yPos + "px)";
+    savedNote.style.width = wdth.toString() + 'px';
+    savedNote.style.height = hght.toString() + 'px';
+
+    document.body.appendChild(savedNote);
+}
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 function getNoteDiv() {
     let noteDiv = document.createElement('div');
     noteDiv.setAttribute('class', 'noteDiv');
@@ -17,6 +85,7 @@ function setRandomNotePosition(newNote) {
 }
 
 function setDragEvent(newNote, randomPositions) {
+
     let draggedElement;
     let grabPointY;
     let grabPointX;
@@ -63,6 +132,8 @@ function setDragEvent(newNote, randomPositions) {
         draggedElement = null;
         grabPointX = null;
         grabPointY = null;
+
+        saveNotes();
     };
 
     newNote.addEventListener('mousedown', onDragStart, false);
@@ -111,7 +182,33 @@ function getNewNote() {
     return newNote;
 }
 
+function saveNotes() {
+    let notes = document.getElementsByClassName('noteDiv');
+    let notesArray = [];
+
+    for (let index = 0; index < notes.length; index++) {
+        let position = notes[index].getBoundingClientRect();
+        let note = {
+            title: notes[index].childNodes[0].childNodes[0].value,
+            content: notes[index].childNodes[1].value,
+            width: Math.round(notes[index].offsetWidth),
+            height: Math.round(notes[index].offsetHeight),
+            xPosition: Math.round(position.left),
+            yPosition: Math.round(position.top)
+        };
+        notesArray[index] = note;
+    }
+
+    localStorage.setItem('notes', JSON.stringify(notesArray));
+}
+
 document.getElementById('addNote').addEventListener('click', function () {
     let newNote = getNewNote();
     document.body.appendChild(newNote);
+}, false);
+document.getElementsByClassName('.noteTopBar input').addEventListener('keyup', function () {
+    saveNotes();
+}, false);
+document.getElementsByClassName('content').addEventListener('keyup', function () {
+    saveNotes();
 }, false);
